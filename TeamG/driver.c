08e273c82c1f15abe39e4edfb7a15ee3b0b7b985
msajlowe <43071930+msajlowe@users.noreply.github.com>
2020-11-22 00:48:37 -0600

    Add files via upload
    added to driver.c file

--- TeamG/driver.c
+++ TeamG/driver.c
@@ -1,6 +1,8 @@
 // Team G\
 // Amanda Lowe, Josh Minton, Brady Prince
 
+#include <stdio.h>
+#include <string.h>
 
 void readFile() {
 }
@@ -9,5 +11,34 @@ void operations() {
 }
 
 int main() {
+	int nMin = 3;
+	int mMin = 3;
+	int nMax = 5;
+	int mMax = 6;
+	
+	bool trainer == true;
+	bool cust == true;
+	
+	for (int i = nMin; i <= nMax; i++) {
+		for (int j = mMin; j <= mMax; j++) {
+		// if customer arrives to fitness center
+		// no trainer is available
+		// customer goes to waiting room to take couch
+		while (!trainer) {
+			// if all couches occupied, customer leaves
+			if (cust) {
+				cust = false;
+			}
+		}
+
+		// if trainer becomes available
+		// goes to waiting room on electric unicycle scooter
+		// pick customer based on FCFS
+
+		// if customer arrives and trainer is available
+		// trainer immediately assists walk in
+		}
+	}
+	
 
 }
