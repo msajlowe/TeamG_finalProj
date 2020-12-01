745827405802a24f43a58f16178d7a9677bdc748
msajlowe <43071930+msajlowe@users.noreply.github.com>
2020-11-30 22:24:25 -0600

    Add files via upload

--- TeamG/driver.c
+++ TeamG/driver.c
@@ -4,71 +4,9 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+#include <sync.c>
 
 using namespace std;
-
-// Function to find the waiting time for all processes
-void findWaitingTime(int processes[], int n, int bt[], int wt[], int at[]) {
-    int service_time[n];
-    service_time[0] = 0;
-    wt[0] = 0;
- 
-    // calculating waiting time
-    for (int i = 1; i < n ; i++)
-    {
-        // Add burst time of previous processes
-        service_time[i] = service_time[i-1] + bt[i-1];
- 
-        // Find waiting time for current process =
-        // sum - at[i]
-        wt[i] = service_time[i] - at[i];
- 
-        // If waiting time for a process is in negative
-        // that means it is already in the ready queue
-        // before CPU becomes idle so its waiting time is 0
-        if (wt[i] < 0)
-            wt[i] = 0;
-    }
-}
- 
-// Function to calculate turn around time
-void findTurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[]) {
-    // Calculating turnaround time by adding bt[i] + wt[i]
-    for (int i = 0; i < n ; i++)
-        tat[i] = bt[i] + wt[i];
-}
- 
-// Function to calculate average waiting and turn-around
-// times.
-void findavgTime(int processes[], int n, int bt[], int at[]) {
-    int wt[n], tat[n];
- 
-    // Function to find waiting time of all processes
-    findWaitingTime(processes, n, bt, wt, at);
- 
-    // Function to find turn around time for all processes
-    findTurnAroundTime(processes, n, bt, wt, tat);
- 
-    // Display processes along with all details
-    cout << "Processes " << " Burst Time " << " Arrival Time "
-         << " Waiting Time " << " Turn-Around Time "
-         << " Completion Time \n";
-    int total_wt = 0, total_tat = 0;
-    for (int i = 0 ; i < n ; i++)
-    {
-        total_wt = total_wt + wt[i];
-        total_tat = total_tat + tat[i];
-        int compl_time = tat[i] + at[i];
-        cout << " " << i+1 << "\t\t" << bt[i] << "\t\t"
-             << at[i] << "\t\t" << wt[i] << "\t\t "
-             << tat[i]  <<  "\t\t " << compl_time << endl;
-    }
- 
-    cout << "Average waiting time = "
-         << (float)total_wt / (float)n;
-    cout << "\nAverage turn around time = "
-         << (float)total_tat / (float)n;
-}
 
 int main() {
     // n = trainers
@@ -77,6 +15,7 @@ int main() {
 	int mMin = 3;
 	int nMax = 5;
 	int mMax = 6;
+	int useCouch = 0;
 	
 	bool trainer == true;
 	bool cust == true;
@@ -84,34 +23,59 @@ int main() {
     
     char onCouch = "Customer is on couch";
     char trainerOnUni = "Trainer goes to waiting room on electric unicycle scooter";
+    char trainerAss = "Trainer assists";
 	
-	for (int i = nMin; i <= nMax; i++) {
-		for (int j = mMin; j <= mMax; j++) {
+	    for (int i = 0; i < 100; i++) {
+
             // if customer arrives to fitness center
             // no trainer is available
+
             while (!trainer) {
                 // if all couches occupied, customer leaves
-                if (cust) {
-                    cust = false;
+                if (couch < mMin || couch > mMax) {
+                    cust == false;
                 }
+
                 // customer goes to waiting room to take couch
-                else if (cust && couch) {
-                    printf("%c %d\n", onCouch, m[j]);
+                else if (cust) {
+                    printf("%c %d\n", onCouch, i);
+					useCouch++;
+					if (useCouch > mMax) {
+						cust[i] == false;
+					}
+					
                 }
             }
 
             // if trainer becomes available
             while (trainer) {
-                // goes to waiting room on electric unicycle scooter
-                printf ("%c\n" trainerOnUni);
-                // pick customer based on FCFS
-
+				// pick customer based on FCFS
                 // if customer arrives and trainer is available
                 // trainer immediately assists walk in
+               if (cust) {
+					printf("%c\n", trainerAss);
+					if (useCouch >= mMin || useCouch <= mMax) {
+						sync.unfair(useCouch);
+					}
+                
+					// goes to waiting room on electric unicycle scooter
+					printf("%c\n", trainerOnUni);
+					if (useCouch >= mMin || useCouch <= mMax) {
+						sync.deadlock(useCouch, trainer, cust);
+					}
+					if (!trainer) {
+						sync.starvation(useCouch, trainer);
+					}
+            	}
+                    
+                    
+                }
+
+                
             }
+        }
 		
-		}
-	}
+return 0;
 	
 
 }
--- TeamG/sync.c
+++ TeamG/sync.c
@@ -1,17 +1,39 @@
 // sync.c
 
-void deadlock () {
-
+void deadlock (int a, bool b, bool c) {
+	// if trainer does not see a client
+	// will go play with phone
+	if (a != 0) {
+		b == true;
+		c == false;
+		printf("Trainer cannot find client.\n");
+	}
+	return 0;
 }
 
-void starvation () {
-
+void starvation (int a, bool b) {
+	// trainer helping other clients with higher priority
+	// client stays waiting indefinitely 
+	if (a != 0) {
+		b == false;
+		// client is unknown
+		printf("All trainers are busy.");
+	}
+	return 0;
 }
 
-void unfair() {
-
+void unfair(int a, bool b, bool c) {
+	// trainer spots client come in and helps
+	// without checking waiting room
+	if (a != 0) {
+		b == false;
+		c == true;
+		printf("The available trainer got sidetracked by another client");
+	}
+	
+	return 0;
 }
 
 void avoidance () {
-
+	// if deadlock is not detected
 }
