73d0d49eec1ec4c454540b9c32882fcf835ef0b7
BradyPrince <72418077+BradyPrince@users.noreply.github.com>
2020-12-04 23:16:20 -0600

    Update sync.c

--- TeamG/sync.c
+++ TeamG/sync.c
@@ -1,21 +1,66 @@
 // sync.c
 
-void deadlock() 
+void detectDeadlock(int i, int* arrivalTimes, int* inWaitingRoom, int numOfCustomers) 
 {
-
+	
 }
+void solveDeadlock()
+{
+	
+}
+
 
 void starvation() 
 {
 
 }
-
-void unfair() 
+void solveStarvation() 
 {
 
 }
 
+
+int detectUnfair(int m, int i, int* arrivalTimes, int* inWaitingRoom, int numOfCustomers) 
+{
+	int numberInWaitingRoom; 
+	for(int j = 0; j < numOfCustomers; j++)
+	{
+		if(inWaitingRoom[j] == 1)
+		{
+			numberInWaitingRoom++;
+		}
+	}
+	if(numberInWaitingRoom < m) return 0; 
+	for(int j = 0; j < numOfCustomers; j++)
+	{
+		if(arrivalTime[i] <= arrivalTime[j] && inWaitingRoom[j])
+		{
+			printf("Unfair Scedualling detected");
+			return 1;
+		}
+	}
+	return 0;
+}
+void SolveUnfair(int i, int* arrivalTimes, int* inWaitingRoom, int numOfCustomers) 
+{
+	for(int j = 0; j < numOfCustomers; j++)
+	{
+		if(arrivalTime[i] < arrivalTime[j] && inWaitingRoom[j])
+		{
+			inWaitingRoom[i] = 1;
+			inWaitingRoom[j] = 0;
+			printf("Unfair Scedualling Solved");
+			return;
+		}
+	}
+}
+
+
 void avoidance() 
+{
+
+}
+void SolveAvoidance() 
 {
 
 }
