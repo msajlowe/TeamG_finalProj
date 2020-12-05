94095d882b3914d8181515a5b07789b364d634a5
BradyPrince <72418077+BradyPrince@users.noreply.github.com>
2020-12-05 03:20:02 -0600

    Update sync.c

--- TeamG/sync.c
+++ TeamG/sync.c
@@ -1,22 +1,38 @@
 // sync.c
 
-void detectDeadlock(int i, int* arrivalTimes, int* inWaitingRoom, int numOfCustomers) 
+int detectDeadlock(sem_t semaphor, int trainerInWaitingRoom) 
 {
-	
+	int numberInWaitingRoom; 
+	sem_getValue(&semaphor, &numberInWaitingRoom);
+	if(trainerInWaitingRoom == 1 && abs(numberInWaitingRoom) == 0)
+	{
+		printf("Deadlock Detected");
+		return 1;
+	}
+	return 0;
 }
 void solveDeadlock()
 {
-	
+	printf("Deadlock Solved");
+	return;
 }
 
 
-void starvation() 
+void detectStarvation(sem_t semaphor, int trainerInWaitingRoom) 
 {
-
+	int numberInWaitingRoom; 
+	sem_getValue(&semaphor, &numberInWaitingRoom);
+	if(trainerInWaitingRoom == 1 && abs(numberInWaitingRoom) >= 0)
+	{
+		printf("Starvation Detected");
+		return 1;
+	}
+	return 0;
 }
 void solveStarvation() 
 {
-
+	printf("Starvation Solved");
+	return;
 }
 
 
@@ -35,13 +51,13 @@ int detectUnfair(int m, int i, int* arrivalTimes, int* inWaitingRoom, int numOfC
 	{
 		if(arrivalTime[i] <= arrivalTime[j] && inWaitingRoom[j])
 		{
-			printf("Unfair Scedualling detected");
+			printf("Unfair Scedualling Detected");
 			return 1;
 		}
 	}
 	return 0;
 }
-void SolveUnfair(int i, int* arrivalTimes, int* inWaitingRoom, int numOfCustomers) 
+void solveUnfair(int i, int* arrivalTimes, int* inWaitingRoom, int numOfCustomers) 
 {
 	for(int j = 0; j < numOfCustomers; j++)
 	{
@@ -56,11 +72,3 @@ void SolveUnfair(int i, int* arrivalTimes, int* inWaitingRoom, int numOfCustomer
 }
 
 
-void avoidance() 
-{
-
-}
-void SolveAvoidance() 
-{
-
-}
